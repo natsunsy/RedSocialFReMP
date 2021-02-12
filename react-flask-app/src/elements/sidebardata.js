38207b2af1d60c65ec3641e1bed786b003bb0c50
AlexChungA <59289797+AlexChungA@users.noreply.github.com>
2021-02-12 00:50:41 -0500

    Obtener perfil de usuario

--- react-flask-app/src/elements/sidebardata.js
+++ react-flask-app/src/elements/sidebardata.js
@@ -3,7 +3,9 @@ import * as AiIcons from 'react-icons/ai';
 import * as IoIcon from 'react-icons/io';
 import * as IoIcons from 'react-icons/io5';
 import PersonIcon from '@material-ui/icons/Person';
-
+const sessionStr = localStorage.getItem("session")
+const sessionJson = JSON.parse(sessionStr)
+const userId = sessionJson.user._id
 export const SidebarData = [
     {
         title: 'Inicio',
@@ -19,7 +21,7 @@ export const SidebarData = [
     },
     {
         title: 'Perfil',
-        path: '/perfil',
+        path: '/perfil/'+userId,
         icon: <PersonIcon/>,
         cName: 'nav-text'
     },
